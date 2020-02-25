b3f86b094668d21878a2e09e4f2a0dfdb8e6721a
Said <msaid.bensadoune@gmail.com>
2020-02-25 20:43:14 +0100

    [fix] fix RestController

--- back-end/src/main/java/com/org/prj/resources/UserResource.java
+++ back-end/src/main/java/com/org/prj/resources/UserResource.java
@@ -6,16 +6,16 @@ package com.org.prj.resources;
 import com.org.prj.dto.UserDto;
 import com.org.prj.services.external.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import javax.ws.rs.Consumes;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import java.util.List;
 
-@Controller
+@RestController
 @RequestMapping("/user")
 @Consumes({ MediaType.APPLICATION_JSON })
 @Produces({ MediaType.APPLICATION_JSON })
